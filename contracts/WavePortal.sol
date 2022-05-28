01975dabc11b94739eb4b18eb47983c81952c6e6
vaibhavxeni <81296814+vaibhavxeni@users.noreply.github.com>
2022-05-29 00:11:21 +0800

    added waves feature

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,7 +5,27 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal {
+    uint256 public totalWaves=0;
+    address[] public contractAddress;
     constructor() {
-        console.log("Yo yo, I am a contract and I am smart");
+        console.log("Learning To Code Web3");
     }
+
+    function wave() public {
+        totalWaves += 1;
+        console.log("%s has waved!", msg.sender);
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We have %d total waves!", totalWaves);
+        return totalWaves;
+    }
+
+    function contractDeployed() public  {
+      contractAddress.push(msg.sender);
+      //contractAddress[totalWaves]=msg.sender;
+      console.log("%s added to array!",contractAddress[totalWaves]);
+    }
+
+
 }
