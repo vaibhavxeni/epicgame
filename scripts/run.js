7eceff5f20513038dbed39bbc13d1c64e3f3979f
vaibhavxeni <81296814+vaibhavxeni@users.noreply.github.com>
2022-06-03 21:42:43 +0800

    sending money to waiver

--- scripts/run.js
+++ scripts/run.js
@@ -1,40 +1,40 @@
 const main = async () => {
-  const [owner, randomPerson] = await hre.ethers.getSigners();
-    const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
-    const waveContract = await waveContractFactory.deploy();
-    await waveContract.deployed();
-    console.log("Contract deployed to:", waveContract.address);
-    console.log("Contract deployed by:", owner.address);
-    await waveContract.contractDeployed();
+  const waveContractFactory = await hre.ethers.getContractFactory('WavePortal');
+  const waveContract = await waveContractFactory.deploy({
+    value: hre.ethers.utils.parseEther('0.1'),
+  });
+  await waveContract.deployed();
+  console.log('Contract addy:', waveContract.address);
 
-    let waveCount;
-    waveCount = await waveContract.getTotalWaves();
+  let contractBalance = await hre.ethers.provider.getBalance(
+    waveContract.address
+  );
+  console.log(
+    'Contract balance:',
+    hre.ethers.utils.formatEther(contractBalance)
+  );
 
-    let waveTxn = await waveContract.wave();
-    await waveTxn.wait();
+  let waveTxn = await waveContract.wave('A message!');
+  await waveTxn.wait();
 
-    waveCount = await waveContract.getTotalWaves();
-    await waveContract.contractDeployed();
+  contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
+  console.log(
+    'Contract balance:',
+    hre.ethers.utils.formatEther(contractBalance)
+  );
 
-    waveTxn = await waveContract.connect(randomPerson).wave();
-    await waveTxn.wait();
-  
-    waveCount = await waveContract.getTotalWaves();
-    await waveContract.contractDeployed();
+  let allWaves = await waveContract.getAllWaves();
+  console.log(allWaves);
+};
 
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
 
-
-  };
-  
-  const runMain = async () => {
-    try {
-      await main();
-      process.exit(0); // exit Node process without error
-    } catch (error) {
-      console.log(error);
-      process.exit(1); // exit Node process while indicating 'Uncaught Fatal Exception' error
-    }
-    // Read more about Node exit ('process.exit(num)') status codes here: https://stackoverflow.com/a/47163396/7974948
-  };
-  
-  runMain();
+runMain();
